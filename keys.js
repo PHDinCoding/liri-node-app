0b903a17f20c2212ac78828fd5efde586af21175
Aashish Panvelkar <Aashish Panvelkar>
2017-03-24 21:54:18 -0400

    Added initial files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'm4MmKjbxpGb5xWy2y5HIANrJC',
+  consumer_secret: '76WfEjPpJxEU82A5opmpmc1CvEx0DnvJggUMpeO9ptzjfC2D7j',
+  access_token_key: '845444784365940738-bmMyfPSlQBVEMAoaMNfaGgWnUKY9gY3',
+  access_token_secret: 'Km7g5iFAEnITi1k9aOi3BCu8w7pSVEOEHffVC1P2JUysZ',
+}
