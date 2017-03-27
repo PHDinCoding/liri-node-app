ed6c1842a7c1356ba6f299b7bc5398d27b5c8d53
Aashish Panvelkar <AashishP@Aashishs-MBP.lan>
2017-03-26 21:25:50 -0400

    update1

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-console.log('this is loaded');
+// console.log('this is loaded');
 
 exports.twitterKeys = {
   consumer_key: 'm4MmKjbxpGb5xWy2y5HIANrJC',
--- liri.js
+++ liri.js
@@ -1,3 +1,68 @@
-var keys = require("./keys.js");
+var command1 = process.argv[2];
+var command2 = process.argv[3];
 
-console.log(keys);
+var keys = require('./keys.js');
+var Twitter = require('twitter');
+var fs = require('fs');
+var spotify = require('spotify');
+
+// var twitterCommand = "my_tweets";
+// console.log(keys);
+// console.log(keys.twitterKeys.consumer_key);
+
+
+
+//Twitter keys instantiation.....
+var client = new Twitter({
+  consumer_key: keys.twitterKeys.consumer_key,
+  consumer_secret: keys.twitterKeys.consumer_secret,
+  access_token_key: keys.twitterKeys.access_token_key,
+  access_token_secret: keys.twitterKeys.access_token_secret
+});
+
+// console.log(client);
+
+switch(command1){
+    //This is a twitter API at work
+case "my_tweets":
+{    
+    client.get('statuses/home_timeline', function(error, tweets, response) {
+    if (!error) {
+    //  console.log(tweets);
+    //!!!Using this to get the tweets in a stringify format so I can make sense of this..... Nothing to see here...
+    //  fs.appendFile('json.txt', JSON.stringify(tweets), (err) => {
+    //     if (err) throw err;
+    //     console.log('The "data to append" was appended to file!');
+    // });
+    for(i=0; i<tweets.length; i++){
+
+        console.log(tweets[i].text);
+        
+    }
+    
+    
+  }
+});
+break;
+}
+case "spotify-this-song": 
+{
+    
+spotify.search({ type: 'track', query: command2 }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    console.log(JSON.stringify(data));
+    // Do something with 'data' 
+        fs.appendFile('spotify_json.json', JSON.stringify(data), (err) => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+    });
+
+});
+break;
+}
+}
+
+
