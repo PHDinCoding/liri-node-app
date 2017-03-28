7c94f4b0f0c54e2a3f1e9ee6f6973f43ecca7f81
Aashish Panvelkar <AashishP@Aashishs-MBP.lan>
2017-03-27 21:40:46 -0400

    'Finished Spotify & Twitter portion'

--- liri.js
+++ liri.js
@@ -1,16 +1,11 @@
 var command1 = process.argv[2];
 var command2 = process.argv[3];
+var aOfBaseKey = "0hrBpAOgrt8RXigk83LLNE";
 
 var keys = require('./keys.js');
 var Twitter = require('twitter');
 var fs = require('fs');
 var spotify = require('spotify');
-
-// var twitterCommand = "my_tweets";
-// console.log(keys);
-// console.log(keys.twitterKeys.consumer_key);
-
-
 
 //Twitter keys instantiation.....
 var client = new Twitter({
@@ -20,13 +15,13 @@ var client = new Twitter({
   access_token_secret: keys.twitterKeys.access_token_secret
 });
 
-// console.log(client);
+// var twitterCommand = "my_tweets";
+// console.log(keys);
+// console.log(keys.twitterKeys.consumer_key);
 
-switch(command1){
-    //This is a twitter API at work
-case "my_tweets":
-{    
-    client.get('statuses/home_timeline', function(error, tweets, response) {
+//When my-tweets gets passed, this function gets called
+var twitterFunction = function(){
+client.get('statuses/home_timeline', function(error, tweets, response) {
     if (!error) {
     //  console.log(tweets);
     //!!!Using this to get the tweets in a stringify format so I can make sense of this..... Nothing to see here...
@@ -39,21 +34,20 @@ case "my_tweets":
         console.log(tweets[i].text);
         
     }
-    
-    
   }
 });
-break;
-}
-case "spotify-this-song": 
-{
+};
 
+//When spotify-this-song gets passed, this function gets called
+var spotifyFunction = function(){
 spotify.search({ type: 'track', query: command2 }, function(err, data) {
     if ( err ) {
         console.log('Error occurred: ' + err);
         return;
     }
     // console.log(JSON.stringify(data));
+    
+    
     var randomNum = Math.floor(Math.random() * data.tracks.items.length);
 
     var artist = JSON.stringify((data).tracks.items[randomNum].album.artists[0].name);
@@ -72,8 +66,76 @@ spotify.search({ type: 'track', query: command2 }, function(err, data) {
     // });
 
 });
-break;
+};
+
+var theSign = function(){
+    spotify.lookup({ type: 'track', id:aOfBaseKey }, function(err, data) {
+        
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    // console.log("This is from theSign function");
+    // console.log(JSON.stringify(data));
+    // var randomNum = Math.floor(Math.random() * data.tracks.items.length);
+    
+    // console.log(data);
+    var artist = data.album.artists[0].name;
+    var song = data.name;
+    var preview = data.preview_url;
+    var album = data.album.name;
+     
+     console.log("\n");
+     console.log('"'+song +'"'+ " by " + artist);
+     console.log("Preview of the song: " + preview);
+     console.log("Album: " + album);
+     console.log("\n");
+    
+    
+// // Just for my use, to parse JSON for my sanity......I know.....
+//     fs.writeFile('spotify_json.json', JSON.stringify(data), (err) => {
+//     // fs.writeFile('spotify_json.json', JSON.stringify(song + " by " + artist), (err) => {
+//         if (err) throw err;
+//         console.log('The "data to append" was appended to file!');
+//     });
+
+});
+};
+
+
+
+// console.log(client);
+
+switch(command1){
+    //This is a twitter API at work
+case "my-tweets":
+{    
+    twitterFunction(); 
+        break;
+}
+    //This is spotify API at work
+case "spotify-this-song": 
+{
+ //Check to see if the 4th element of the command line arg is null if that's false
+ //then the AND condition fails and goes to the Else part.
+    if( process.argv[3] == null  && command1 === "spotify-this-song")
+    {
+        theSign();
+        
+        
+    }else
+    {
+        spotifyFunction();
+    }
+        break;
+}
+
+default:{
+    console.log("\n");
+    console.log("Please check your command, you might have misspelled something?");
+    console.log("\n");
 }
 }
+
 
 
